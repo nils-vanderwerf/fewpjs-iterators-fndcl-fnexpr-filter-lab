9d2d9ca46e1c514a6b896b55643e063ca25a3d55
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-11 22:37:34 +1100

    Find matching names return

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (drivers, name) {
+const result = drivers.filter(n => n === name);
+return result;
+}
+
+console.log(findMatching(drivers, 'Bobby'))
