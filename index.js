7bf064c8f6ea862f259b4fdd31500c99a9512f29
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-12 09:32:40 +1100

    Passed all tests

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby', 'bobby']
+
 
 function findMatching (drivers, name) {
-const result = drivers.filter(n => n === name);
-return result;
+const loweCase = drivers.filter(n => n.toLowerCase() === name.toLowerCase());
+return loweCase;
 }
 
-console.log(findMatching(drivers, 'Bobby'))
+function fuzzyMatch (drivers, letters) {
+    const matchChars = drivers.filter(n => n.substring(0, 2) === letters);
+    console.log(matchChars);
+    return matchChars;
+}
+
+function matchName(drivers, name) {
+    const objName = drivers.filter(n => n.name === name);
+    console.log(objName);
+    return objName;
+}
+
+console.log(matchName(drivers, 'Bobby'));
